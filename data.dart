7c9c26665d1f6aa9a8ba53a4e79998aad409e946
Nida4178 <98307755+Nida4178@users.noreply.github.com>
2022-03-05 21:59:50 +0530

    Add files via upload

---
+++ data.dart
@@ -0,0 +1,7 @@
+Map<String, String> studentDetails = {
+  "": "Enter the Roll number to see student's details",
+  "1": "NAME - AASTHA \n\n COURSE - B.TECH \n\n FEE - SUBMITTED \n\n ADMISSION YEAR - 2022",
+  "2": "NAME - AADYA \n\n COURSE - B.TECH \n\n FEE - SUBMITTED \n\n ADMISSION YEAR - 2022",
+  "3": "NAME - AAKANKSHA \n\n COURSE - B.TECH \n\n FEE - SUBMITTED \n\n ADMISSION YEAR - 2022",
+  "4": "NAME - NIDA \n\n COURSE - B.TECH \n\n FEE - SUBMITTED \n\n ADMISSION YEAR - 2022",
+};
